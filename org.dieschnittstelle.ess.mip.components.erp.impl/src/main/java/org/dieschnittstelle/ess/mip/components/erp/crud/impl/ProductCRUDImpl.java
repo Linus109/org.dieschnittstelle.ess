511cc35063a82be26e847ee9890af40efe659975
Linus109 <linus.waldowsky@yahoo.de>
2025-05-31 18:48:22 +0200

    MIP+JPA2 theoretically done, but it won't fill the database

--- org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/impl/ProductCRUDImpl.java
+++ org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/impl/ProductCRUDImpl.java
@@ -1,16 +1,27 @@
 package org.dieschnittstelle.ess.mip.components.erp.crud.impl;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.Campaign;
 import org.dieschnittstelle.ess.mip.components.erp.crud.api.ProductCRUD;
+import org.dieschnittstelle.ess.utils.interceptors.Logged;
 
 import java.util.List;
 
 import static org.dieschnittstelle.ess.utils.Utils.show;
 
 @ApplicationScoped
+@Transactional
+@Logged
 public class ProductCRUDImpl implements ProductCRUD {
+
+    @Inject
+    @EntityManagerProvider.ERPDataAccessor
+    private EntityManager em;
 
     public ProductCRUDImpl() {
         show("constructor(): ProductCRUDImpl created!");
@@ -18,24 +29,26 @@ public class ProductCRUDImpl implements ProductCRUD {
 
     @Override
     public AbstractProduct createProduct(AbstractProduct prod) {
-        show("createProduct(): " + prod);
+        show("createProduct() in IMPL: " + prod);
+        em.persist(prod);
         return prod;
     }
 
     @Override
     public List<AbstractProduct> readAllProducts() {
         show("readAllProducts()");
-        return List.of();
+        Query q = em.createQuery("select p from AbstractProduct p");
+        return q.getResultList();
     }
 
     @Override
     public AbstractProduct updateProduct(AbstractProduct update) {
-        return null;
+        return em.merge(update);
     }
 
     @Override
     public AbstractProduct readProduct(long productID) {
-        return null;
+        return em.find(AbstractProduct.class, productID);
     }
 
     @Override
