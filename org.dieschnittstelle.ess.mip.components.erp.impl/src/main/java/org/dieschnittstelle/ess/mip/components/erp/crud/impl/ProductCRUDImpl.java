a0c138f05f9205a6aebd49d27f9bc3958b27850a
SimonB97 <102378134+SimonB97@users.noreply.github.com>
2025-06-02 10:20:57 +0200

    MIP+JPA 2 done

--- org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/impl/ProductCRUDImpl.java
+++ org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/impl/ProductCRUDImpl.java
@@ -4,39 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.Campaign;
 import org.dieschnittstelle.ess.mip.components.erp.crud.api.ProductCRUD;
+import org.dieschnittstelle.ess.mip.components.erp.crud.impl.EntityManagerProvider.ERPDataAccessor;
 
 @ApplicationScoped
 @Transactional
 public class ProductCRUDImpl implements ProductCRUD {
 
+	@Inject
+	@ERPDataAccessor
+	private EntityManager entityManager;
+
 	@Override
 	public AbstractProduct createProduct(AbstractProduct prod) {
+		entityManager.persist(prod);
 		return prod;
 	}
 
 	@Override
 	public List<AbstractProduct> readAllProducts() {
-		return new ArrayList<>();
+		return entityManager.createQuery("SELECT p FROM AbstractProduct p", AbstractProduct.class)
+				.getResultList();
 	}
 
 	@Override
 	public AbstractProduct updateProduct(AbstractProduct update) {
-		return update;
+		return entityManager.merge(update);
 	}
 
 	@Override
 	public AbstractProduct readProduct(long productID) {
-		return null;
+		return entityManager.find(AbstractProduct.class, productID);
 	}
 
 	@Override
 	public boolean deleteProduct(long productID) {
-		return true;
+		AbstractProduct product = entityManager.find(AbstractProduct.class, productID);
+		if (product != null) {
+			entityManager.remove(product);
+			return true;
+		}
+		return false;
 	}
 
 	@Override
